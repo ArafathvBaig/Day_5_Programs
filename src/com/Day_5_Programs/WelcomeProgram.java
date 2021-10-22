1f43b9ec96019b021691504b1a6152f20b2bc254
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-22 14:00:32 +0530

    Adding Welcome Program

---
+++ src/com/Day_5_Programs/WelcomeProgram.java
@@ -0,0 +1,12 @@
+package com.Day_5_Programs;
+
+public class WelcomeProgram 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Day 5 Basic Core and Functional Programs");
+
+	}
+
+}
