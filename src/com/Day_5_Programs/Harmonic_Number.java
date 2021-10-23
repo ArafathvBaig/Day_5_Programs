9bdfbf3bd067115a88a0dbb1f8cffd7cc8e7b538
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-23 14:27:35 +0530

    Adding File to Branch-4

---
+++ src/com/Day_5_Programs/Harmonic_Number.java
@@ -0,0 +1,27 @@
+package com.Day_5_Programs;
+
+import java.util.Scanner;
+
+public class Harmonic_Number
+{
+	public static void main(String[] args) 
+	{
+		double n;
+		double temp=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter N: ");
+		n=sc.nextInt();	
+		if(n>0)
+		{
+			for(double i=1; i<=n; i++)
+			{
+				temp=temp+(1/i);
+			}
+			System.out.println("The Harmonic value of "+n+"th term is: "+temp);
+		}
+		else
+		{
+			System.out.println("Wrong Input");
+		}
+	}
+}
